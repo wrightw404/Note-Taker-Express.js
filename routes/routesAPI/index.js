ce1f5089a1fe7d0d626cc319ece062d7793c71cd
Will <wrightw404@gmail.com>
2022-09-30 16:19:16 -0700

    small path changes in notes.js. continued working on index.js in routesAPI

--- routes/routesAPI/index.js
+++ routes/routesAPI/index.js
@@ -2,4 +2,18 @@ const router = require('express').Router();
 //calls the two exported function from notes.js
 const {noteCreate, noteDelete} = require('../../lib/notes')
 const notes = require('../../db/db.json')
+const uuidv1 = require('uuid/v1');
+
 //get post and delete /notes
+
+router.get('/notes', (req, res) => {
+
+})
+
+router.post('/notes', (req, res) => {
+
+})
+
+router.delete('/notes/:id', (req, res) => {
+
+})
