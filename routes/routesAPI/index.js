5171e99520d924e3c1e1baa1f3072684a839d9de
Will <wrightw404@gmail.com>
2022-09-30 16:30:16 -0700

    finished index.js file in routesAPI

--- routes/routesAPI/index.js
+++ routes/routesAPI/index.js
@@ -7,13 +7,20 @@ const uuidv1 = require('uuid/v1');
 //get post and delete /notes
 
 router.get('/notes', (req, res) => {
-
+    let savedResults = notes;
+    res.json(savedResults);
 })
 
 router.post('/notes', (req, res) => {
-
+    req.body.id = uuidv1();
+    const createdNote = noteCreate(req.body, notes);
+    res.json(createdNote);
 })
 
 router.delete('/notes/:id', (req, res) => {
-
+    noteDelete(notes, req.params.id);
+    res.redirect('');
 })
+
+module.exports = 
+router;
