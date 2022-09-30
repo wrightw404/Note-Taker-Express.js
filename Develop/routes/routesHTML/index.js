b66a22d24a45fec5f078952521f6fb4eab31c02c
Will <wrightw404@gmail.com>
2022-09-30 15:15:30 -0700

    fixed paths for /notes and *

--- Develop/routes/routesHTML/index.js
+++ Develop/routes/routesHTML/index.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 //get /notes and * 
 router.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
 router.get('/notes', (req,res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
 
 module.exports = 
