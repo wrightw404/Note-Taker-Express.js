dd70f3d1519e12bb954d448875a58e0c262f9e1c
Will <wrightw404@gmail.com>
2022-09-30 10:00:49 -0700

    deleted path and fs calls as they were un-needed

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const fs = require('fs');
+//const path = require('path');
+//const fs = require('fs');
 const PORT = 3001;
 const express = require('express');
 const app = express();
