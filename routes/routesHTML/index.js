142f2052963d1341ee2bd09305d38ef97d3c2b4c
Will <wrightw404@gmail.com>
2022-09-30 17:43:32 -0700

    fixed bug of not being able to access localhost:3001/notes

--- routes/routesHTML/index.js
+++ routes/routesHTML/index.js
@@ -2,12 +2,17 @@ const router = require('express').Router();
 const path = require('path');
 
 //get /notes and * 
-router.get('*', (req,res) => {
+router.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
 router.get('/notes', (req,res) => {
     res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
+
+router.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
 
 module.exports = router;
